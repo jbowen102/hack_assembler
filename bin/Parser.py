79f0e87551adcb96cdb90ac8ff6df036d814aa6b
JB (TXT Comp) <jjbowen19@gmail.com>
2019-04-18 13:07:21 -0400

    Create Command class and code more of Parser class

--- bin/Parser.py
+++ bin/Parser.py
@@ -11,7 +11,7 @@ class Parser(object):
 
         # initiate mapping of symbols to their mem locations
         # variable mappings will be added to this list as they're encountered.
-        self.symbols = {"SP": 0, "LCL": 1, "ARG": 2,
+        self.variables = {"SP": 0, "LCL": 1, "ARG": 2,
                         "THIS": 3, "THAT": 4, "R0": 0, "R1": 1, "R2": 2,
                         "R3": 3, "R4": 4, "R5": 5, "R6": 6, "R7": 7, "R8": 8,
                         "R9": 9, "R10": 10, "R11": 11, "R12": 12, "R13": 13,
@@ -35,7 +35,7 @@ class Parser(object):
                     # read in all characters until close parenthesis.
                     # don't increment counter or transcribe to new list.
                     symbol = stripped_line[1:].split(")")[0]
-                    self.jump_labels[symbol] = line_num + 1
+                    self.jump_labels[symbol] = line_num
                 else:
                     print("\t%d - %s" % (line_num, stripped_line))
                     line_num += 1
@@ -43,11 +43,52 @@ class Parser(object):
 
         self.stripped_file = AC_only_list
 
+        self.command_dict = {}
+        line = 0
+        for command_line in self.stripped_file:
+            current_command = Command(command_line, self.jump_labels, self.variables)
+            self.command_dict[line] = current_command
+            line += 1
+
     def jump_map(self):
         return self.jump_labels
 
-    def variable_mem_map(self):
+    def variable_map(self):
         return self.variables
 
     def write_parsed_file(self):
+        # write .hack file
         pass
+
+
+class Command(object):
+    """Class representing a single parsed command from .asm file."""
+
+    def __init__(self, command_line, jump_dict, variable_dict):
+        self.raw_command = command_line
+        self.jump_map = jump_dict
+        self.variable_map = variable_dict
+
+        # code to determine what type of command the object is
+        # code to parse the specific command into its parts (dest, jump, etc.)
+        if self.raw_command[0] == "@":
+            self.type = "A"
+            raw_address = self.raw_command[1:]
+
+            # code to look for symbol in variable dictionary or interpret as
+            # raw number
+            # if not either one, then add variable to variable dictionary,
+            # linking it to the next available mem slot.
+
+        else:
+            self.type = "C"
+
+
+
+
+
+    def asm_text(self):
+        return self.raw_command
+
+    def command_type(self):
+        return self.type
